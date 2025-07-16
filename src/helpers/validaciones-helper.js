ee7fcf9d1d71e7daec10db859a53a7886ce36041
48313612 <167318076+48313612@users.noreply.github.com>
2025-07-16 09:38:56 -0300

    Delete src directory

--- src/helpers/validaciones-helper.js
+++
@@ -1,8 +0,0 @@
-export function validarText(text) {
-    return typeof text === 'string' && text.length >= 3;
-  }
-  
-  export function validarID(id) {
-    return !isNaN(id) && Number(id) > 0;
-  }
-  
